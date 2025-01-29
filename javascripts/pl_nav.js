f3e1a8bd4b9706dbcfabd2442c5d370905be7ab3
Juan Miguel Lahorra <lahorrajm@gmail.com>
2025-01-29 16:02:56 +0800

    added logos, updated pages, and css

--- javascripts/pl_nav.js
+++ javascripts/pl_nav.js
@@ -1,1 +1,9 @@
-document.getElementById('myFunction')
+function myFunction() {
+    var element = document.getElementById("html-sec");
+    element.classList.toggle("html-sec");
+    element.onclick = function() {
+        if (html-sec.style.display == "none") {
+            html-sec.style.display == "block";
+        }
+    }
+}
